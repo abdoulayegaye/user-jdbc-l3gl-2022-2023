9d27cccb0a9a488aa2a4511e88291a6b61b4783c
Abdoulaye GAYE <layegaye001@gmail.com>
2023-01-19 20:08:48 +0000

    Projet Java avec JDBC Second commit

--- src/presentation/Main.java
+++ src/presentation/Main.java
@@ -22,10 +22,33 @@ public class Main {
         }else{
             System.out.println("Insertion échouée !");
         }*/
-        List<User> listeUsers = iUser.list();
+        /*List<User> listeUsers = iUser.list();
         System.out.println("Liste des utilisateurs");
         for (User u : listeUsers) {
             System.out.println(u.getPrenom() + " - " + u.getNom() + " [ " + u.getEmail() + " ] ");
-        }
+        }*/
+        /*User user = new User();
+
+        user.setId(2);
+        user.setNom("FAYE");
+        user.setPrenom("Babs");
+        user.setEmail("babacar@groupeisi.com");
+        user.setPassword("toto@1234");
+
+        if(iUser.update(user) == 1){
+            System.out.println("Utilisateur modifié avec Succés !");
+        }else{
+            System.out.println("Modification échouée !");
+        }*/
+
+        /*if(iUser.delete(7) == 1){
+            System.out.println("Utilisateur supprimé avec Succés !");
+        }else{
+            System.out.println("Suppression échouée !");
+        }*/
+
+        User u = iUser.get(1);
+        System.out.println("L'utilisateur dont son id=1 est : " +
+                u.getPrenom() + " " + u.getNom() + " ["+u.getEmail()+"]");
     }
 }
--- src/sn/exo/controller/UserImpl.java
+++ src/sn/exo/controller/UserImpl.java
@@ -33,12 +33,34 @@ public class UserImpl implements IUser{
 
     @Override
     public int update(User u) {
-        return 0;
+        String sql = "UPDATE user SET nom=?, prenom=?, email=?, password=? WHERE id=?";
+        try{
+            db.initPrepar(sql);
+            db.getPstm().setString(1, u.getNom());
+            db.getPstm().setString(2, u.getPrenom());
+            db.getPstm().setString(3, u.getEmail());
+            db.getPstm().setString(4, u.getPassword());
+            db.getPstm().setInt(5, u.getId());
+            ok = db.executeMaj();
+            db.closeConnection();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return ok;
     }
 
     @Override
     public int delete(int id) {
-        return 0;
+        String sql = "DELETE FROM user WHERE id=?";
+        try{
+            db.initPrepar(sql);
+            db.getPstm().setInt(1, id);
+            ok = db.executeMaj();
+            db.closeConnection();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return ok;
     }
 
     @Override
@@ -57,6 +79,7 @@ public class UserImpl implements IUser{
                 user.setPassword(rs.getString(5));
                 users.add(user);
             }
+            db.closeConnection();
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -65,6 +88,23 @@ public class UserImpl implements IUser{
 
     @Override
     public User get(int id) {
-        return null;
+        User user = null;
+        String sql = "SELECT * FROM user WHERE id=?";
+        try {
+            db.initPrepar(sql);
+            db.getPstm().setInt(1, id);
+            rs = db.executeSelect();
+            if(rs.next()){
+                user = new User();
+                user.setId(rs.getInt("id"));
+                user.setNom(rs.getString("nom"));
+                user.setPrenom(rs.getString("prenom"));
+                user.setEmail(rs.getString("email"));
+                user.setPassword(rs.getString("password"));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return user;
     }
 }
